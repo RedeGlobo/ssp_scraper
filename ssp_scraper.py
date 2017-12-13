116062900c33a43550b87ec36e2d3a4786b1cbcc
Priscilla Lusie <priscilla.lusie@tvglobo.com.br>
2017-12-13 22:01:45 +0000

    closes #1 adicionado a licença

--- ssp_scraper.py
+++ ssp_scraper.py
@@ -1,8 +1,12 @@
-# coding: utf-8
 #!/usr/bin/env python
+# coding: utf-8
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2017 Rede Globo
 
 __author__ = "Rodrigo Esteves e Priscilla Lusie"
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 import logging
 import os
